759347b64cab6a62be7d15a8c49ffa1f8083a7d0
xSyrax123 <99150043+xSyrax123@users.noreply.github.com>
2022-02-23 14:23:27 -0300

    Delete invalid_input.py

--- tic_tac_toe/invalid_input.py
+++
@@ -1,6 +0,0 @@
-class InvalidInput(Exception):
-    """Indicates an invalid user input."""
-    def __init__(self, message):
-        """Sets the message to the user."""
-        super().__init__(message)
-        self.message = message
