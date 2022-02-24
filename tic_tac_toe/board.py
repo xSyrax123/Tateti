79136260e953b4b7f689be6f318f976410b0b971
xSyrax123 <99150043+xSyrax123@users.noreply.github.com>
2022-02-24 10:52:03 -0300

    Add files via upload

---
+++ tic_tac_toe/board.py
@@ -0,0 +1,29 @@
+from constants import FIELD_EMPTY, WAYS_TO_WIN
+from itertools import chain
+
+
+class Board: 
+    def __init__(self):
+        self._board = [" "] * 9
+        
+    def __str__(self):
+        """Return a string with game board."""
+        return "{}║{}║{}\n═╬═╬═\n{}║{}║{}\n═╬═╬═\n{}║{}║{}".format(*chain(self._board))
+
+    @property
+    def board(self):
+        return self._board
+
+    @board.setter
+    def board(self, board):
+        self._board = board
+        
+    def board_full(self):
+        """Return True if every space on the board
+        has been taken. Otherwise return False."""
+        return FIELD_EMPTY not in self.board
+
+    def check_win(self, board, letter):
+        """Given a board and a player letter
+        return True if that player has won."""
+        return any(board[s] == [letter, letter, letter] for s in WAYS_TO_WIN)
